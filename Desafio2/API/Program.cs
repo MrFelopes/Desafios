4afcb482745e85f4e920d0a21e81faeec4245ad4
Felipe Redivo Fragoso <79218872+MrFelopes@users.noreply.github.com>
2025-03-07 18:29:40 -0300

    Comentário importante em Program.cs

--- Desafio2/API/Program.cs
+++ Desafio2/API/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("PermitirTudo", policy =>
     {
-        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500") //Caso necessário, alterar para a porta designada do Live Server de index.html
               .AllowAnyMethod()
               .AllowAnyHeader()
               .AllowCredentials();
